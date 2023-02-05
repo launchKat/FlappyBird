dede72de08b1a1f15a9c916d619e6404b20833cb
launchKat <109763974+launchKat@users.noreply.github.com>
2023-02-05 17:00:51 -0600

    got working, fixed color, next add icons and background

--- bird.js
+++ bird.js
@@ -1,38 +1,38 @@
-const birdElem = document.querySelector("data-bird");
+const birdElem = document.querySelector("[data-bird]");
 const BIRD_SPEED = 0.5;
 const JUMP_DURATION = 125;
 let timeSinceLastJump = Number.POSITIVE_INFINITY;
 
-export function setupBird(){
-    setTop(window.innerHeight / 2);
-    document.removeEventListener("keydown", handleJump);
-    document.addEventListener("keydown", handleJump);
+export function setupBird() {
+  setTop(window.innerHeight / 2);
+  document.removeEventListener("keydown", handleJump);
+  document.addEventListener("keydown", handleJump);
 }
 
-export function updateBird(delta){
-    if(timeSinceLastJump < JUMP_DURATION){
-    setTop(getTop() - BIRD_SPEED * delta); //"-" means bird will move down "+ means bird will move up"
-    }else{
-        setTop(getTop() + BIRD_SPEED * delta);
-    }
+export function updateBird(delta) {
+  if (timeSinceLastJump < JUMP_DURATION) {
+    setTop(getTop() - BIRD_SPEED * delta);
+  } else {
+    setTop(getTop() + BIRD_SPEED * delta);
+  }
 
-    timeSinceLastJump += delta;
+  timeSinceLastJump += delta;
 }
 
-export function getBirdRect(){
-    return birdElem.getBoundingClientRect();
+export function getBirdRect() {
+  return birdElem.getBoundingClientRect();
 }
 
-function setTop(top){
-    birdElem.setPropety("--bird-top", top);
+function setTop(top) {
+  birdElem.style.setProperty("--bird-top", top);
 }
 
-function getTop(){
-    return parseFloat(getComputedStyle(birdElem).getPropertyValue("--bird-top"));
+function getTop() {
+  return parseFloat(getComputedStyle(birdElem).getPropertyValue("--bird-top"));
 }
 
-function handleJump(e){
-    if(e.code !== "Space") return
+function handleJump(e) {
+  if (e.code !== "Space") return
 
-    timeSinceLastJump = 0;
+  timeSinceLastJump = 0;
 }
--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-import { updateBird, setupBird, getBirdRect } from './bird.js'
+import { updateBird, setupBird, getBirdRect } from "./bird.js";
 import {
-    updatePipes,
-    setupPipes,
-    getPassedPipesCount,
-    getPipeRects
-} from ".pipe.js";
+  updatePipes,
+  setupPipes,
+  getPassedPipesCount,
+  getPipeRects,
+} from "./pipe.js";
 
-document.addEventListener("keypress", handleStart, {once: true});
-const title = document.querySelector("[data-title");
-const subtitle = document.querySelector("[data-title]");
+document.addEventListener("keypress", handleStart, { once: true });
+const title = document.querySelector("[data-title]");
+const subtitle = document.querySelector("[data-subtitle]");
 
-let lastTime;
-function updateLoop(time){
-    if(lastTime == null){
-        lastTime = time;
-        window.requestAnimationFrame(updateLoop);
-        return
-    }
-    const delta = time - lastTime;
-    updateBird(delta);
-    updatePipes(delta);
-    if(checkLose()) return handleLose();
+let lastTime
+function updateLoop(time) {
+  if (lastTime == null) {
     lastTime = time;
     window.requestAnimationFrame(updateLoop);
+    return;
+  }
+  const delta = time - lastTime;
+  updateBird(delta);
+  updatePipes(delta);
+  if (checkLose()) return handleLose();
+  lastTime = time;
+  window.requestAnimationFrame(updateLoop);
 }
 
-function checkLose(){
-    const birdRect = getBirdRect();
-    const insidePipe = getPipeRects().some(rect => isCollision(birdRect, rect));
-    const outsideWorld = birdRect.top < 0 || birdRect.bottom > window.innerHeight
-    return outsideWorld || insidePipe;
+function checkLose() {
+  const birdRect = getBirdRect();
+  const insidePipe = getPipeRects().some(rect => isCollision(birdRect, rect));
+  const outsideWorld = birdRect.top < 0 || birdRect.bottom > window.innerHeight;
+  return outsideWorld || insidePipe;
 }
 
-function isCollision(rect1, rect2){
-    return (
-        rect1.left < rect2.rght &&
-        rect1.top < rect2.bottom &&
-        rect1.right > rect2.left &&
-        rect1.bottom > rect2.top
-    );
+function isCollision(rect1, rect2) {
+  return (
+    rect1.left < rect2.right &&
+    rect1.top < rect2.bottom &&
+    rect1.right > rect2.left &&
+    rect1.bottom > rect2.top
+  );
 }
 
-function handleStart(){
-title.classList.add("hide");
-setupBird();
-setupPipes();
-lastTime = null;
-window.requestAnimationFrame(updateLoop);
+function handleStart() {
+  title.classList.add("hide");
+  setupBird();
+  setupPipes();
+  lastTime = null;
+  window.requestAnimationFrame(updateLoop);
 }
 
-function handleLose(){
-    setTimeout(() => {
-        title.classList.remove("hide");
-        subtitle.classList.remove("hide");
-        subtitle.textContent = `${getPassedPipesCount()} Pipes`;
-        document.addEventListener("keypress", handleStart, {once: true})
-    }, 100);
+function handleLose() {
+  setTimeout(() => {
+    title.classList.remove("hide")
+    subtitle.classList.remove("hide")
+    subtitle.textContent = `${getPassedPipesCount()} Pipes`
+    document.addEventListener("keypress", handleStart, { once: true })
+  }, 100);
 }
